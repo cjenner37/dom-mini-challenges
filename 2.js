f6341746723f81622f3cc45bdf100bb371bd1a43
Cameron Jenner <cjenner37@gmail.com>
2017-05-31 14:53:44 -0600

    2 and 3 done

--- 2.js
+++ 2.js
@@ -2,3 +2,9 @@
 Create an HTML page with a large element on the page that says "Don't hover over me" inside of it. When 
 you hover over the element, send an alert to the user that says, "Hey, I told you not to hover over me!"
 */
+
+var box = document.getElementById('box');
+
+box.addEventListener('mouseover', function(e){
+	alert("Hey! I told you not to hover over me!!");
+});
--- 3.js
+++ 3.js
@@ -3,3 +3,8 @@ Create an HTML page with javascript that listens for a keypress.
 	When the user presses that key, the text of the h1 should show the value of the key they have pressed.
 	Example: If the user presses "J", the text inside the h1 should be "J".
 */
+
+document.addEventListener('keypress', function(e){
+	console.log(e);
+	document.getElementById('box').innerText = "You pressed: " + e.key;
+});
